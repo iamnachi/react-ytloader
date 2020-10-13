2b77024e113e9ab64635aabb1d4d0e331c5e4817
“nachi <nachisinbox@gmail.com>
2020-10-13 18:13:48 +0530

    Updates

---
+++ precache-manifest.7c2df7a7e89b71d3bdccee63b949b83b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a72f07644829e8834acf9fc94c7789b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "963d53bd7973f22ec34e",
+    "url": "./static/css/main.6ef19bea.chunk.css"
+  },
+  {
+    "revision": "de3b58a20b2c2f0e2e51",
+    "url": "./static/js/2.aeda8105.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.aeda8105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963d53bd7973f22ec34e",
+    "url": "./static/js/main.dd1a07a4.chunk.js"
+  },
+  {
+    "revision": "ca5addb7ea2cbdee5963",
+    "url": "./static/js/runtime-main.9df25195.js"
+  }
+]);
